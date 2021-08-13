9b9a55ad71f62c88163c419fc5840f5a30dabece
Mukhtar Ali <mukhtarali.chaudhari@excellentwebworld.in>
2021-08-13 18:45:18 +0530

    initial commit.

--- Gas 2 You/Controllers/Home Controllers/AddVehicleVC.swift
+++ Gas 2 You/Controllers/Home Controllers/AddVehicleVC.swift
@@ -8,13 +8,58 @@
 import UIKit
 
 class AddVehicleVC: BaseVC {
-
+    
+    
+    
+    @IBOutlet weak var lblMake: themeLabel!
+    @IBOutlet weak var lblYear: themeLabel!
+    @IBOutlet weak var txtEnterYear: UITextField!
+    @IBOutlet weak var txtEnterMake: UITextField!
+    @IBOutlet weak var lblModel: themeLabel!
+    @IBOutlet weak var txtEnterModel: UITextField!
+    @IBOutlet weak var lblColor: themeLabel!
+    @IBOutlet weak var txtEnterColor: UITextField!
+    @IBOutlet weak var lblLicencePlateNum: themeLabel!
+    @IBOutlet weak var txtLicencePlateNo: UITextField!
+    @IBOutlet weak var btnSave: ThemeButton!
+    
+    let picker: GeneralPickerView = GeneralPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         NavBarTitle(isOnlyTitle: false, isMenuButton: false, title: "Add Vehicle", controller: self)
+        
+        txtEnterYear.isUserInteractionEnabled = false
+        txtEnterMake.isUserInteractionEnabled = false
+        txtEnterModel.isUserInteractionEnabled = false
+        txtEnterColor.isUserInteractionEnabled = false
+        
+        picker.generalPickerDelegate = self
     }
     @IBAction func btnSaveTap(_ sender: ThemeButton) {
         navigationController?.popViewController(animated: true)
     }
+}
+
+extension AddVehicleVC: UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        
+        
+        return false
+    }
+}
+
+extension AddVehicleVC: GeneralPickerViewDelegate{
+    
+    func didTapDone() {
+        print("done button pressed")
+    }
+    
+    func didTapCancel() {
+        print("cancel button pressed")
+    }
+    
 }
---
+++ Gas 2 You/Extension/GeneralPickerView.swift
@@ -0,0 +1,54 @@
+//
+//  GeneralPickerView.swift
+//  Gas 2 You
+//
+//  Created by MacMini on 13/08/21.
+//
+
+import UIKit
+
+protocol GeneralPickerViewDelegate: class {
+    func didTapDone()
+    func didTapCancel()
+}
+
+
+class GeneralPickerView: UIPickerView {
+    
+    public private(set) var toolbar: UIToolbar?
+    public weak var generalPickerDelegate: GeneralPickerViewDelegate?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+
+    private func commonInit() {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        //toolBar.tintColor = .black
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneTapped))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+
+        toolBar.setItems([spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+        self.toolbar = toolBar
+    }
+
+    @objc func doneTapped() {
+        self.generalPickerDelegate?.didTapDone()
+    }
+
+    
+}
+
